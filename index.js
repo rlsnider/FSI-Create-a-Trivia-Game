4cc0eefffc321f17d6b3703e3f3373d2fbd883d8
Ramona Snider <rls2061@yahoo.com>
2022-03-21 02:42:15 -0700

    worked on code for Triva game. code included console log answers for debugging

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your Code Here
+let userName = window.prompt('Please enter your name: ')//"Get users name"
+let userScore = 0
+
+for (let i = 0; i < questions.length; i++) {//"Start your for "
+    let question = questions[i]
+    let userAnswer = window.prompt(question.text)
+    if(userAnswer === question.correctAnswer){
+        userScore = userScore + 10
+        console.log("Correct")
+    }
+    console.log(userAnswer)
+}
+window.alert("your score is: "+userScore)
